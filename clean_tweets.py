b415aec592bd850beec2fa43b7910fa979b9e6ff
Kurush Dubash <dubash.kurush@gmail.com>
2015-07-12 02:36:41 +0000

    add is_tweet_bad boolean method

--- clean_tweets.py
+++ clean_tweets.py
@@ -16,6 +16,10 @@ api = twitter.Api(consumer_key='VzoDn3WeXht6OkS2HvNueflzL',
 explicit_tweets = [] # a list of twitter status objects that we coined as 'bad'
 
 def get_auth_info(consumer_key, consumer_secret, access_token_key, access_token_secret):
+	""" Takes in consumer_key, consumer_secret, access_token_key, access_token_secret
+		from user input
+		Returns respective values
+	"""	
 	return consumer_key, consumer_secret, access_token_key, access_token_secret
 
 def get_user_object(username):
@@ -24,7 +28,6 @@ def get_user_object(username):
 	"""
 	return api.GetUser(screen_name=username)
 
-
 def clean_tweets():
 	""" Looks at the list of twitter IDs in explict_tweet_ids and removes them.
 	"""
@@ -32,3 +35,16 @@ def clean_tweets():
 		api.DestroyStatus(tweet.id)
 		print "Deleting Tweet: " + tweet.text
 		explicit_tweets.remove(tweet)
+
+def is_tweet_bad(tweet):
+	""" tweet : a Twitter Status object
+		checks to see if the tweet contains any bad language referenced
+		from our bad_words list
+		Returns true if tweet contains bad word, false otherwise
+	""" 
+	text = tweet.text
+	tweet_words = text.split()
+	for word in tweet_words:
+		if word in bad_words:
+			return True
+	return False
