beda682d011a05d1cbc510e6184528ae12b8898c
Kurush Dubash <dubash.kurush@gmail.com>
2015-07-12 01:00:15 +0000

    added implementation for production, get_user_object returns a twitter object based on the user name specified

--- clean_tweets.py
+++ clean_tweets.py
@@ -2,14 +2,29 @@ import requests
 import twitter
 import os
 
-# global var for bad words
-bad_words = []
-
-# auth stuff
+# auth stuff for testing purposes
 api = twitter.Api(consumer_key='VzoDn3WeXht6OkS2HvNueflzL',
                       consumer_secret='wAmFnCv4w3o5dV4gBSzCTTJbMaDaUWoMKBrRPesljECUPJMzKW',
                       access_token_key='46310347-WAJLn4UDyfxlupMy7Q1nNoyQ4F4OpjZdjaVCPtCGJ',
                       access_token_secret='FObEKM6jMrYX884j8TlzoywREW5fI1A7ZfSQFa8xY41AB')
+
+
+# global var for bad words
+bad_words = []
+explict_tweet_ids = [] 
+
+def get_auth_info(consumer_key, consumer_secret, access_token_key, access_token_secret):
+	return consumer_key, consumer_secret, access_token_key, access_token_secret
+
+def get_user_object(username):
+	return api.GetUser(screen_name=username)
+
+# # for production
+# api = twitter.Apit(consumer_key=consumer_key, 
+# 					  consumer_secret=consumer_secret, 
+# 					  access_token_key=access_token_key, 
+# 					  access_token_secret=access_token_secret)
+
 
 def get_bad_words():
 	""" Finds all the bad words from a local directory and puts it into our 
@@ -24,3 +39,5 @@ def get_bad_words():
 		        	bad_words.append(word)
 
 get_bad_words()
+
+
